5f816498ff69688ff1e86fc76a45f4904b128f7a
Dmytro <dmitry.dvernikov@gmail.com>
2025-06-26 19:34:51 +0300

    fix vercel

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,6 +1,10 @@
 import Notes from './Notes.client';
 
-export default async function FilteredNotesPage({ params }: { params: { slug?: string[] } }) {
-    const tag = params?.slug?.[0] ?? null;
-    return <Notes tag={tag} />;
+export default async function FilteredNotesPage({
+                                                    params,
+                                                }: {
+    params: { slug?: string[] };
+}) {
+    const tag = params.slug?.[0] ?? null;
+    return <Notes tag={tag}/>;
 }

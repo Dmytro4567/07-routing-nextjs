ea2dda25f6a2f0e9dd127e40e487fabcdb38cfe1
Dmytro <dmitry.dvernikov@gmail.com>
2025-06-27 19:50:48 +0300

    fix bug

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -3,7 +3,7 @@
 import {use} from 'react';
 import {useRouter} from 'next/navigation';
 import Modal from '@/components/Modal/Modal';
-import NotePreview from '@/components/NotePreview/NotePreview';
+import NotePreview from '@/app/@modal/(.)notes/[id]/NotePreview.client';
 
 export default function ModalNotePage({params}: { params: Promise<{ id: string }> }) {
     const {id} = use(params);
